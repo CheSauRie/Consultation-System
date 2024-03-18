10551834eb5dcf18bf97184e34a23314c3b62206
cheSau <hongquanvd1@gmail.com>
2024-03-18 18:15:50 +0700

    update data.sql

--- Backend/data.sql
+++ Backend/data.sql
@@ -501,7 +501,7 @@ UNLOCK TABLES;
 -- Table structure for table `sequelizemeta`
 --
 
-DROP TABLE IF EXISTS `sequelizemeta`;
+DROP TABLE IF EXISTS `SequelizeMeta`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `SequelizeMeta` (
